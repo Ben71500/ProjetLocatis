dbbcf76eecaa16d0a58c55627bb74e81c26417f0
ben <benjaminrandazzo2@hotmail.fr>
2022-06-07 18:10:02 +0200

    Habiter début

---
+++ Locatis/src/MVC/Vue_Statistique.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+import javax.swing.*;
+/**
+ *
+ * @author benja
+ */
+public class Vue_Statistique extends JFrame{
+    private JPanel panneau = new JPanel();
+    private JPanel haut = new JPanel();
+    private JPanel centre = new JPanel();
+    JToolBar toolBar = new JToolBar();
+}
