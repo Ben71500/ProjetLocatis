bfc46d026ceedaa7fe710b706e7b22ec09a71eb9
MV <mathilde.vollet.auditeur@lecnam.net>
2022-06-23 01:21:35 +0200

    debut apparence

--- Locatis/src/Vues/Vue_Connexion.java
+++ Locatis/src/Vues/Vue_Connexion.java
@@ -1,7 +1,9 @@
 package Vues;
 
 import Exceptions.EmptyFieldException;
+import Main.Panneau;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -12,11 +14,11 @@ import javax.swing.*;
  */
 public class Vue_Connexion extends JFrame {
 
-    private JPanel panneau = new JPanel();
-    private JPanel haut = new JPanel();
-    private JPanel centre = new JPanel();
-    private JPanel bas = new JPanel();
-    private JPanel panneau_infos = new JPanel();
+    private Panneau panneau = new Panneau();
+    private Panneau haut = new Panneau();
+    private Panneau centre = new Panneau();
+    private Panneau bas = new Panneau();
+    private Panneau panneau_infos = new Panneau();
     
     private JLabel titre = new JLabel("Connectez-vous !");
     private JLabel login_label = new JLabel("Login : ");
@@ -29,12 +31,16 @@ public class Vue_Connexion extends JFrame {
     
     private JButton connexion = new JButton("Connexion");
     
+    private Color gris = new Color(148,177,195);
+    private ImageIcon logo = new ImageIcon("../logo.jpg");
     /**
      * Modèle de la vue
      */
     public Vue_Connexion() {
         super("Se Connecter");
+        this.setIconImage(logo.getImage());
         
+        //panneau.setBackground(gris);
         panneau.setLayout(new BorderLayout());
         panneau.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         panneau.add(this.haut, BorderLayout.NORTH);
@@ -43,19 +49,26 @@ public class Vue_Connexion extends JFrame {
         
         titre.setFont(new java.awt.Font("Tahoma", 1, 24));
         haut.add(this.titre);
+        //haut.setBackground(new Color(148,177,195));
         
         GridLayout gridlayout=new GridLayout(2,2);
         gridlayout.setHgap(10);
         gridlayout.setVgap(10);
+        //panneau_infos.setBackground(gris);
         panneau_infos.setLayout(gridlayout);
         panneau_infos.add(this.login_label);
         panneau_infos.add(this.login);
         panneau_infos.add(this.mdp_label);
         panneau_infos.add(this.motDePasse);
+        //centre.setBackground(gris);
         centre.setLayout(new BorderLayout());
         centre.add(this.panneau_infos, BorderLayout.CENTER);
         centre.add(this.messageErreur, BorderLayout.SOUTH);
         
+        connexion.setBackground(new Color(108,71,149));
+        connexion.setForeground(Color.white);
+        
+        //bas.setBackground(gris);
         bas.setLayout(new GridLayout(1,2));
         bas.add(this.connexion);
         mdp_oublie.setForeground(new java.awt.Color(51, 51, 255));
